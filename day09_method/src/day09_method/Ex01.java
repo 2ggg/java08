fe7d8230c06230046fb1d586ab63e0970ce2f7ad
2ggg <larunaru0386@gmail.com>
2021-08-05 04:38:40 +0900

    중간 저장

--- day09_method/src/day09_method/Ex01.java
+++ day09_method/src/day09_method/Ex01.java
@@ -18,10 +18,23 @@ class
 - 메소드 또는 함수라 부름
 - 클래스 내부에서 정의
 - 클래스 없이 독립적으로 사용 가능
+
+접근 제한자
+- public : 외부, 내부 접근 모두 허용
+- private : 외부 접근 제한. 내부에서만 사용 가능
+- protected : 상속받은 자식
+- default(접근제한자 X) : 동일한 패키지에서만 접근 가능
  */
+
+public String name, addr, phNum, birthString;
+public int age, stNum;
+
 	public static void main(String[] args) {
-		System.out.println();
-		Scanner scanner = new Scanner(System.in);
-		scanner.next();
+		
+		Ex01 ex01 = new Ex01(); //객체
+		ex01.age = 12345;
+		ex01.name = "안녕하세요";
+		System.out.println(ex01.name);
+		System.out.println(ex01.age);
 	}
 }
